2ec657713819f18076bfad2c1cab0e32f326399b
Praveens_git <praveenn.subu@gmail.com>
2017-02-26 19:38:16 +0530

    Updated Basketball Game to a more functional approach

--- src/university/sprint/BasketballGame.scala
+++ src/university/sprint/BasketballGame.scala
@@ -1,38 +1,51 @@
 package university.sprint
 
+import scala.io.StdIn.readLine
+
 /**
  * @author Praveen
  */
 object BasketballGame {
   def main(args: Array[String]): Unit = {
-    val sc = new java.util.Scanner(System.in);
-    val n = sc.nextInt();
-    val score = new Array[Int](n);
-    for (score_i <- 0 to n - 1) {
-      score(score_i) = sc.nextInt();
-    }
+    val numberOfGames = readLine().toInt
+    val score = List { readLine().split(" ").map(_.toInt) }.flatten
 
-    var highest = score(0)
-    var lowest = score(0)
+    val max = score.tail.scanLeft(score.head) {
+      (acc, num) =>
+        acc max num
+    }.toSet.filter { x => x != score.head }
 
-    val highCount = score.count { x =>
+    val min = score.tail.scanLeft(score.head) {
+      (acc, num) =>
+        acc min num
+    }.toSet.filter { x => x != score.head }
+
+    println(max.size + " " + min.size)
+
+    /*var highest = score(0)
+    var lowest = score(0)*/
+
+    /* val high = score(0).tail.scanLeft(score.head){
+        (acc, num) => acc max num
+    }*/
+
+    /* val highCount = score.count { x =>
       val temp = highest
       if (x > highest) {
        highest = x
-       //println("high " + x)
       }
       x > temp
     }
+    */
 
-    val lowCount = score.count { x =>
+    /* val lowCount = score.count { x =>
       val temp = lowest
       if (x < lowest) {
-        //println("low " + x)
         lowest = x
       } 
       x < temp
     }
 
-    println(highCount + " " + lowCount)
+    println(highCount + " " + lowCount)*/
   }
 }
