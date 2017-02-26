8f6f3fdaedf7a355406e0f5aadbfdf9a112d6a30
Praveens_git <praveenn.subu@gmail.com>
2017-02-26 19:42:19 +0530

    Updated Basketball Game to a more functional approach

--- src/university/sprint/BasketballGame.scala
+++ src/university/sprint/BasketballGame.scala
@@ -3,6 +3,24 @@ package university.sprint
 import scala.io.StdIn.readLine
 
 /**
+ * <b>Problem </b>
+ * Maria plays 'n' games of college basketball in a season. Because she wants to go pro, she tracks her
+ * points scored per game sequentially in an array defined as score = [s0,s1,...sN-1] . After each game i,
+ * she checks to see if score s(i) breaks her record for most or least points scored so far during that season.
+ *
+ * Given Maria's array of 'scores' for a season of n games, find and print the number of times she breaks her 
+ * record for most and least points scored during the season.
+ *
+ * <b>Input Format</b>
+ *
+ * The first line contains an integer denoting 'n' (the number of games).
+ * The second line contains 'n' space-separated integers describing the respective values of s0,s1,...sN-1.
+ *
+ * <b>Output Format</b>
+ *
+ * Print two space-seperated integers describing the respective numbers of times her best (highest) score 
+ * increased and her worst (lowest) score decreased.
+ *
  * @author Praveen
  */
 object BasketballGame {
