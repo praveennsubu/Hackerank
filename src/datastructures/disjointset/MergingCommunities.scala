355d6f7eba067ae7402ab9e6adbe95de03966f0a
Praveen Subramanian <subramanian-p@HCL.COM>
2017-03-03 14:39:27 +0530

    Modified MergingCommunities.scala

--- src/datastructures/disjointset/MergingCommunities.scala
+++ src/datastructures/disjointset/MergingCommunities.scala
@@ -10,21 +10,22 @@ object MergingCommunities {
   def main(args: Array[String]): Unit = {
     val Array(noOfPersons, testCases) = readLine().split(" ").map(_.toInt)
     val persons = Array.tabulate(noOfPersons)(_ + 0)
-    val ranks = Array.fill(noOfPersons)(0) //rank attributes for the nodes in a graph
-    val inputs = Array.fill(testCases) { readLine().split(" ")}
-    val results = new ListBuffer[Int]
-    
-    inputs.foreach { x => 
+    val ranks = Array.fill(noOfPersons)(1) //rank attributes for the nodes in a graph
+    val inputs = Array.fill(testCases) { readLine().split(" ") }
+
+    inputs.foreach { x =>
       val query = x(0)
-      
-      if(query == "Q") {
-         val person = persons(x(1).toInt - 1)
-         results.append(persons.count(_ == person))
+
+      if (query == "Q") {
+        //val person = persons(x(1).toInt - 1)
+        val person = x(1).toInt - 1
+        val root = findRoot(person)
+        println(ranks(root))
       } else {
         val (first, second) = (x(1).toInt - 1, x(2).toInt - 1)
-        union(first,second)
+        union(first, second)
       }
-      
+
       //Union method to merge two subsets if they belong together.
       //Uses ranks to flatten out the graph.
       //All subsets have 0 rank first. For adding two nodes/graphs that have same rank,
@@ -35,16 +36,19 @@ object MergingCommunities {
         val firstRoot = findRoot(first)
         val secondRoot = findRoot(second)
 
-        if (ranks(first) < ranks(second)) {
-          updatePersonRoot(firstRoot, secondRoot).copyToArray(persons)
-        } else if (ranks(first) > ranks(second)) {
-          updatePersonRoot(secondRoot, firstRoot).copyToArray(persons)
+        if (ranks(secondRoot) > ranks(firstRoot)) {
+          persons(firstRoot) = secondRoot
+          ranks(secondRoot) = ranks(secondRoot) + ranks(firstRoot)
+        } else if (ranks(secondRoot) < ranks(firstRoot)) {
+          persons(secondRoot) = firstRoot
+          ranks(firstRoot) = ranks(secondRoot) + ranks(firstRoot)
         } else {
-          updatePersonRoot(secondRoot, firstRoot).copyToArray(persons)
-          ranks(second) = ranks(second) + 1
+          persons(secondRoot) = firstRoot
+          ranks(firstRoot) = ranks(secondRoot) + 1
         }
+
       }
-      
+
       //Implementing Path-Compression when finding root
       //Assigning the root of the graph as the parent of every node in the graph
       def findRoot(index: Int): Int = {
@@ -54,17 +58,17 @@ object MergingCommunities {
         }
         root
       }
-      
+
       //Update the root of a graph with a new parent of the entire graph.
       //This is synonymous to pointing the root of a graph to the root of another graph,
       //(which now becomes the parent of the entire graph)
-      //Uses Scala match to replace oldRoot value with the newRoot
-      def updatePersonRoot(oldRoot: Int, newRoot: Int) = {
-        persons.map[Int, Array[Int]] { case `oldRoot` => newRoot; case x => x }
+      def updatePersonRoot(oldRoot: Int, newRoot: Int) {
+        persons.foreach { x =>
+          if (x == oldRoot) persons(x) = newRoot
+        }
+        //persons.map[Int, Array[Int]] { case `oldRoot` => newRoot; case x => x }
       }
-      
+
     }
-    
-    results.foreach(println)
   }
 }
